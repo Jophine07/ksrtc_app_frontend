6f6bf70ba19ac9e8e87b726c817973951f3e4f47
Jophine07 <jophinepaul@gmail.com>
2024-06-14 06:37:18 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
+import Add from './components/Add';
 
 function App() {
   return (
     <div>
      <Login/>
      <SignUp/>
+     <Add/>
     </div>
   );
 }
---
+++ src/components/Add.jsx
@@ -0,0 +1,65 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+
+const Add = () => {
+    const [data, setData] = useState(
+        {
+            "busName": "",
+            "route": "",
+            "busNo": "",
+            "driver": ""
+        }
+    )
+    const inputHandler = (event) => {
+        setData({ ...data, [event.target.name]: event.target.value })
+    }
+    const readValue = () => {
+        console.log(data)
+        axios.post("http://localhost:8080/add",data).then(
+            (response) => {
+                console.log(response.data)
+                if (response.data.status == "success") {
+                    alert("Added")
+                }
+                else {
+                    alert("Not Added")
+                }
+
+            }
+        )
+    }
+
+    return (
+        <div>
+            <div className="container">
+                <div className="row col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">Bus Name</label>
+                            <input type="text" name="busName" value={data.busName} onChange={inputHandler} className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Route</label>
+                        <input type="text" name="route" value={data.route} onChange={inputHandler} className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Bus Number</label>
+                        <input type="text" name="busNo" value={data.busNo} onChange={inputHandler} className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Driver</label>
+                        <input type="text" name="driver" value={data.driver} onChange={inputHandler} className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button className="btn btn-success" onClick={readValue}>Add</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Add
